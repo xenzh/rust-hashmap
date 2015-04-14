94935f948889c150ff423fc30475a31a1a6f891e
xenzh <xenzh0@gmail.com>
2015-04-14 14:28:11 +0300

    Making tests from main() into test module.\nNote: test 3 still causese crash on win7

--- src/main.rs
+++ src/main.rs
@@ -7,54 +7,13 @@
 mod igors_hashmap;
 
 
+#[cfg(test)]
+mod test;
+
+
 // =======================================================================================
 
 
 fn main() {
     println!("Here we go");
-    
-    println!("\ntest #1 - map insertion, search and mem leaks");
-    {
-        let mut igors_hash1 = igors_hashmap::HashMap::new();
-        igors_hash1.insert(&42, &42);
-        for test_key in 42..44 {
-            let found = igors_hash1.find(&test_key);
-            println!("Find key = {}, value = {}", test_key, found.unwrap_or(-1));
-        }
-    }
-
-    println!("\ntest #2 - collision resolving and mem leaks");
-    {
-        let mut igors_hash2 = igors_hashmap::HashMap::new();
-        for test_key in (0..10).step_by(2) {
-            igors_hash2.insert(&test_key, &(test_key * 10));
-            igors_hash2.insert(&(test_key + 1024 as i32), &(test_key * 100));
-        }
-
-        for test_key in 0..10 {
-            let found = igors_hash2.find(&test_key);
-            println!("Find key = {}, value = {}", test_key, found.unwrap_or(-1));
-        }
-        for test_key in 1024..1034 {
-            let found = igors_hash2.find(&test_key);
-            println!("Find key = {}, value = {}", test_key, found.unwrap_or(-1));
-        }
-    }
-
-    println!("\ntest #3 - string map");
-    {
-        let mut igors_hash3: igors_hashmap::HashMap<String, String> =
-            igors_hashmap::HashMap::new();
-
-        // causes crashes on windows 7
-        // igors_hash3.insert(&"Ihor".to_string(), &"Name".to_string());
-        // igors_hash3.insert(&"Kopeichyk".to_string(), &"Last name".to_string());
-        // igors_hash3.insert(&"Gennadievich".to_string(), &"Second name".to_string());
-        
-        // let found = igors_hash3.find(&"Ihor".to_string());
-        // println!("\"Ihor\" is {}", found.unwrap_or("[not found".to_string()));
-
-        // let found = igors_hash3.find(&"Vasiliy".to_string());
-        // println!("\"Vasiliy\" is {}", found.unwrap_or("[not found]".to_string()));
-    }
 }
---
+++ src/test.rs
@@ -0,0 +1,62 @@
+use super::igors_hashmap;
+
+
+#[test]
+pub fn simple_int_pair_insertion() {
+    let mut igors_hash1 = igors_hashmap::HashMap::new();
+    igors_hash1.insert(&42, &42);
+
+    let found = igors_hash1.find(&42);
+    assert_eq!(found.is_some(), true);
+    assert_eq!(found.unwrap(), 42);
+
+    let not_found = igors_hash1.find(&43);
+    assert_eq!(not_found.is_some(), false);
+}
+
+#[test]
+pub fn collision_handling() {
+    let mut igors_hash2 = igors_hashmap::HashMap::new();
+    for test_key in (0..10).step_by(2) {
+        igors_hash2.insert(&test_key, &(test_key * 10));
+        igors_hash2.insert(&(test_key + 1024 as i32), &(test_key * 100));
+    }
+
+    for test_key in 0..10 {
+        let found = igors_hash2.find(&test_key);
+        if test_key % 2 == 0 {
+            assert_eq!(found.is_some(), true);
+            assert_eq!(found.unwrap(), test_key * 10);
+        } else {
+            assert_eq!(found.is_some(), false);
+        }
+    }
+
+    for test_key in 1024..1034 {
+        let found = igors_hash2.find(&test_key);
+        if test_key % 2 == 0 {
+            assert_eq!(found.is_some(), true);
+            assert_eq!(found.unwrap(), (test_key - 1024) * 100);
+        } else {
+            assert_eq!(found.is_some(), false);
+        }
+    }
+}
+
+#[test]
+pub fn simple_string_pair_insertion() {
+    let mut igors_hash3: igors_hashmap::HashMap<String, String> =
+        igors_hashmap::HashMap::new();
+
+    // causes crashes on windows 7
+    igors_hash3.insert(&"Ihor".to_string(), &"Name".to_string());
+    igors_hash3.insert(&"Kopeichyk".to_string(), &"Last name".to_string());
+    igors_hash3.insert(&"Gennadievich".to_string(), &"Second name".to_string());
+    
+    let found = igors_hash3.find(&"Ihor".to_string());
+    assert_eq!(found.is_some(), true);
+    assert_eq!(found.unwrap(), "Name".to_string());
+
+    let found = igors_hash3.find(&"Vasiliy".to_string());
+    assert_eq!(found.is_some(), false);
+}
